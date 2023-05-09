8c988ce8e7b5d32e7cb2135f33ff161ba0b28995
Samuel Augusto Alvarado <118860772+Samuelzoom14@users.noreply.github.com>
2023-05-09 15:53:24 -0300

    Correcciones + JS Navbar

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
-const nombre = document.getElementById("name")
-const mail = document.getElementById("mail")
-const password = document.getElementById("password")
-const form = document.getElementById("form")
-const parrafo = document.getElementById("warnings")
+const usuario = document.getElementById("mailModal");
+const passwordModal = document.getElementById("passwordModal");
+const modal = document.getElementById("login");
+const inicioSesion = document.getElementById("inicioSesion");
+const adminSection = document.getElementById('adminSection');
 
-form.addEventListener("submit", e=>{
-    e.preventDefault()
-    let warnings = ""
-    let entrar = false
-    let regexMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    parrafo.innerHTML = ""
-    if(nombre.value.length <6){
-        warnings += `El nombre no es valido <br>`
-        entrar = true
-    }
-    if(!regexMail.test(mail.value)){
-        warnings += `El mail no es valido <br>`
-        entrar = true
-    }
-    if(password.value.length <6){
-        warnings += `La contraseña no es valida <br>`
-        entrar = true
-    }
-    if(entrar){
-        parrafo.innerHTML = warnings
-    }
-    else{
-        parrafo.innerHTML = "Enviado"
-        window.location = "index.html"
-    }
-})
+modal.addEventListener("submit", e => {
+  e.preventDefault();
+  let usuarioAdmin = "segundoproyecto@gmail.com";
+  let passwordAdmin = "peliscode";
+  if (usuario.value === usuarioAdmin && passwordModal.value === passwordAdmin) {
+    adminSection.style.display = 'block';
+    inicioSesion.textContent = 'Administrador';
+    localStorage.setItem('isAdmin', 'true');
+    window.location.href = "index.html";
+  } else {
+    alert("no coincide");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  let isAdmin = localStorage.getItem('isAdmin');
+  if (isAdmin === 'true') {
+    adminSection.style.display = 'block';
+    inicioSesion.textContent = 'Administrador';
+  }
+});
+
+inicioSesion.addEventListener('click', () => {
+  if (inicioSesion.textContent === 'Administrador') {
+    localStorage.removeItem('isAdmin'); 
+    inicioSesion.textContent = 'Inicio de sesion';
+    adminSection.style.display = 'none';
+    } 
+});
 
 
-
-
-  
-  
 
 
---
+++ registro.js
@@ -0,0 +1,41 @@
+const nombre = document.getElementById("name")
+const mail = document.getElementById("mail")
+const password = document.getElementById("password")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    parrafo.innerHTML = ""
+    if(nombre.value.length <6){
+        warnings += `El nombre no es valido <br>`
+        entrar = true
+    }
+    if(!regexMail.test(mail.value)){
+        warnings += `El mail no es valido <br>`
+        entrar = true
+    }
+    if(password.value.length <6){
+        warnings += `La contraseña no es valida <br>`
+        entrar = true
+    }
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }
+    else{
+        parrafo.innerHTML = "Enviado"
+        window.location = "index.html"
+    }
+})
+
+
+
+
+
+  
+  
+
+
